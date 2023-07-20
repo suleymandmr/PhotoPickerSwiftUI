32f9f129eec0d2c3ed3792328f3ad9aa3438720c
Süleyman Demir <suleymandemir75@hotmail.com>
2023-07-20 21:43:05 +0300

    Initial Commit

---
+++ PhotoPickerSwiftUI/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by eyüp yaşar demir on 18.07.2023.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ContentView: View {
+    
+    @State var selectedItems : [PhotosPickerItem] = []
+    @State var data : Data?
+    
+    var body: some View {
+        VStack {
+            if let data = data {
+                if let selectedImage = UIImage(data:  data) {
+                    Spacer()
+
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .frame(width: 300,height: 250,alignment: .center)
+                        
+                }
+            }
+                Spacer()
+
+                    PhotosPicker(selection: $selectedItems, maxSelectionCount: 1,  matching: .images) {
+                        Text("Select Image")
+                    
+                    }.onChange(of: selectedItems) { newValue in
+                        guard let item = selectedItems.first else {
+                            return
+                        }
+                        item.loadTransferable(type: Data.self) { result in
+                            switch result {
+                            case .success(let data):
+                                if let data = data {
+                                    self.data = data
+                                }
+                            case .failure(let error):
+                                print(error)
+                            }
+                            
+                        }
+                    }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by eyüp yaşar demir on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
